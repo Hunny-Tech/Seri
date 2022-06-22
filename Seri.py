eb3667e4feba9ece7005934baf8a892f22e9bfdc
Hunny-Tech <85615596+Hunny-Tech@users.noreply.github.com>
2022-06-22 22:40:39 +0500

    Create Seri.py
    git clone https://github.com/Hunny-Tech/Seri.git
    ls
    cd Seri
    ls
    python Seri.py

---
+++ Seri.py
@@ -0,0 +1,33 @@
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    print("\n\x1b[1;92m First See Video How To Use");time.sleep(2)
+
+    print(' Subscribe ❤ ');time.sleep(0.5)
+
+    print(' Like & Comment ❤ ');time.sleep(0.5)
+
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100023252198062')
+
+    from Hunny Xd
+
+    HUNNY()
+
+elif bit == '32bit':
+
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
